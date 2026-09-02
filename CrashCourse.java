f5ce7c1569ed48a99dfd5846670be6d52e9b0557
UnderDEVeloped <stanley20282146@student.nhvweb.net>
2026-09-02 12:35:05 +0000

    crashcourse

---
+++ CrashCourse.java
@@ -0,0 +1,66 @@
+public class CrashCourse {    //class header
+    
+    public static void main(String[] args) {
+
+        // comments
+
+        /*muiltiline
+        comment
+        here
+        */
+
+        System.out.print("hello");
+        System.out.println("goop"); //autoprints on new line
+        System.out.println("im gumpin it");
+
+        //Varibles
+
+        //Primitive Variables
+        int a; //integer
+        double b; // decimal/float
+        boolean c; //true or false
+
+        a = 4;
+        b = 5.5;
+        c = false; //lowercase booleans
+
+        // Arithmetic operators
+        // + - / *
+        // +=  -=  /=  %=
+
+        int d = 3;
+        d += 7;
+
+        System.out.println("d = " + d);
+
+        // incremend and decements by 1
+        // --  ++
+        d--;
+        d++;
+        d++;
+        System.out.println("d = " + d);
+
+        // comparisions retursns true of false
+        // < > <= >= == !=
+
+        System.out.println(4 > 3);
+        System.out.println(12 != 8);
+        System.out.println(8 == 3);
+
+        // logical operators
+        // NOT: !    AND:  &&   OR: ||
+
+        boolean e = false; boolean f = true;
+
+        //predict
+        System.out.println(!e); //True
+        System.out.println(e && f); //False
+        System.out.println(e || f); //True
+        System.out.println(e || f && !e); //True
+
+        System.out.println(e && f); // short circusts after checking e
+
+
+    }
+
+}
